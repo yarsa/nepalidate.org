8702777553fad8bf3544587403f992bb0f5ecaa9
Nj Subedi <3161806+njsubedi@users.noreply.github.com>
2025-01-20 20:03:11 +0545

    simplify

---
+++ public/index.js
@@ -0,0 +1,271 @@
+export const bsMonths = [
+  "बैशाख",
+  "जेठ",
+  "आषाढ",
+  "श्रावण",
+  "भदौ",
+  "असोज",
+  "कार्तिक",
+  "मंसिर",
+  "पौष",
+  "माघ",
+  "फाल्गुन",
+  "चैत्र",
+];
+export const bsDays = ["आईत", "सोम", "मंगल", "बुध", "बिही", "शुक्र", "शनि"];
+export const bsDaysFull = [
+  "आइतवार",
+  "सोमवार",
+  "मंगलवार",
+  "बुधवार",
+  "बिहिवार",
+  "शुक्रवार",
+  "शनिवार",
+];
+export const adDays = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+export const adDaysFull = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thusday",
+  "Friday",
+  "Saturday",
+];
+export const nepaliNumbers = ["०", "१", "२", "३", "४", "५", "६", "७", "८", "९"];
+export const bsMonthUpperDays = [
+  [30, 31],
+  [31, 32],
+  [31, 32],
+  [31, 32],
+  [31, 32],
+  [30, 31],
+  [29, 30],
+  [29, 30],
+  [29, 30],
+  [29, 30],
+  [29, 30],
+  [30, 31],
+];
+export const extractedBsMonthData = [
+  [
+    0, 1, 1, 22, 1, 3, 1, 1, 1, 3, 1, 22, 1, 3, 1, 3, 1, 22, 1, 3, 1, 19, 1, 3,
+    1, 1, 3, 1, 2, 2, 1, 3, 1,
+  ],
+  [
+    1, 2, 2, 2, 2, 2, 2, 1, 3, 1, 3, 1, 2, 2, 2, 3, 2, 2, 2, 1, 3, 1, 3, 1, 2,
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 3, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1,
+    3, 1, 2, 2, 2, 2, 2, 1, 1, 1, 2, 2, 2, 2, 2, 1, 3, 1, 1, 2,
+  ],
+  [
+    0, 1, 2, 1, 3, 1, 3, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2,
+    1, 3, 1, 3, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 3, 1, 3, 1, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 1, 3, 1, 3, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 3, 1, 1, 2,
+  ],
+  [
+    1, 2, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1,
+    3, 1, 3, 1, 2, 2, 2, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 2, 2, 2, 1, 3, 1, 3,
+    1, 3, 1, 3, 1, 3, 1, 3, 2, 2, 1, 3, 1, 2, 2, 2, 1, 2,
+  ],
+  [59, 1, 26, 1, 28, 1, 2, 1, 12],
+  [
+    0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 3, 1, 3, 1, 3, 1, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 1, 3, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 3, 1, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2, 5, 1, 1, 2, 2, 1, 3, 1, 2, 1, 2,
+  ],
+  [
+    0, 12, 1, 3, 1, 3, 1, 5, 1, 11, 1, 3, 1, 3, 1, 18, 1, 3, 1, 3, 1, 18, 1, 3,
+    1, 3, 1, 27, 1, 2,
+  ],
+  [
+    1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 3, 1, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+    1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 1, 2, 2, 2, 15, 2, 4,
+  ],
+  [
+    0, 1, 2, 2, 2, 2, 1, 3, 1, 3, 1, 3, 1, 2, 2, 2, 3, 2, 2, 2, 1, 3, 1, 3, 1,
+    3, 1, 2, 2, 2, 2, 2, 2, 2, 1, 3, 1, 3, 1, 3, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+    1, 3, 1, 3, 1, 2, 2, 2, 15, 2, 4,
+  ],
+  [
+    1, 1, 3, 1, 3, 1, 14, 1, 3, 1, 1, 1, 3, 1, 14, 1, 3, 1, 3, 1, 3, 1, 18, 1,
+    3, 1, 3, 1, 3, 1, 14, 1, 3, 15, 1, 2, 1, 1,
+  ],
+  [
+    0, 1, 1, 3, 1, 3, 1, 10, 1, 3, 1, 3, 1, 1, 1, 3, 1, 3, 1, 10, 1, 3, 1, 3, 1,
+    3, 1, 3, 1, 14, 1, 3, 1, 3, 1, 3, 1, 3, 1, 10, 1, 20, 1, 1, 1,
+  ],
+  [
+    1, 2, 2, 1, 3, 1, 3, 1, 3, 1, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2, 1, 3, 1, 3,
+    1, 3, 1, 2, 2, 2, 2, 2, 2, 2, 1, 3, 1, 3, 1, 3, 1, 3, 1, 2, 2, 2, 2, 2, 2,
+    2, 1, 3, 1, 3, 1, 20, 3,
+  ],
+];
+export const minBsYear = 1970;
+export const maxBsYear = 2100;
+export const minAdDateEqBsDate = {
+  ad: {
+    year: 1913,
+    month: 3,
+    date: 13,
+  },
+  bs: {
+    year: 1970,
+    month: 1,
+    date: 1,
+  },
+};
+
+export const toDevanagariDigits = (number) => {
+  return String(number)
+    .split("")
+    .map((ed) => CalendarData.nepaliNumbers[Number(ed)])
+    .join("");
+};
+
+export const getTotalDaysNumFromMinBsYear = (bsYear, bsMonth, bsDate) => {
+  if (bsYear < CalendarData.minBsYear || bsYear > CalendarData.maxBsYear) {
+    return null;
+  }
+
+  let daysNumFromMinBsYear = 0;
+  const diffYears = bsYear - CalendarData.minBsYear;
+  for (let month = 1; month <= 12; month++) {
+    if (month < bsMonth) {
+      daysNumFromMinBsYear += getMonthDaysNumFormMinBsYear(
+        month,
+        diffYears + 1
+      );
+    } else {
+      daysNumFromMinBsYear += getMonthDaysNumFormMinBsYear(month, diffYears);
+    }
+  }
+
+  if (bsYear > 2085 && bsYear < 2088) {
+    daysNumFromMinBsYear += bsDate - 2;
+  } else if (bsYear === 2085 && bsMonth > 5) {
+    daysNumFromMinBsYear += bsDate - 2;
+  } else if (bsYear > 2088) {
+    daysNumFromMinBsYear += bsDate - 4;
+  } else if (bsYear === 2088 && bsMonth > 5) {
+    daysNumFromMinBsYear += bsDate - 4;
+  } else {
+    daysNumFromMinBsYear += bsDate;
+  }
+
+  return daysNumFromMinBsYear;
+};
+
+export const getMonthDaysNumFormMinBsYear = (bsMonth, yearDiff) => {
+  let yearCount = 0;
+  let monthDaysFromMinBsYear = 0;
+  if (yearDiff === 0) return 0;
+
+  const bsMonthData = CalendarData.extractedBsMonthData[bsMonth - 1];
+  for (let i = 0; i < bsMonthData.length; i++) {
+    if (bsMonthData[i] === 0) {
+      continue;
+    }
+    const bsMonthUpperDaysIndex = i % 2;
+    if (yearDiff > yearCount + bsMonthData[i]) {
+      yearCount += bsMonthData[i];
+      monthDaysFromMinBsYear +=
+        CalendarData.bsMonthUpperDays[bsMonth - 1][bsMonthUpperDaysIndex] *
+        bsMonthData[i];
+    } else {
+      monthDaysFromMinBsYear +=
+        CalendarData.bsMonthUpperDays[bsMonth - 1][bsMonthUpperDaysIndex] *
+        (yearDiff - yearCount);
+      yearCount = yearDiff - yearCount;
+      break;
+    }
+  }
+
+  return monthDaysFromMinBsYear;
+};
+
+export const getBsMonthDays = (bsYear, bsMonth) => {
+  let yearCount = 0;
+  if (bsYear === null || bsMonth === null) {
+    return null;
+  }
+  const totalYears = bsYear + 1 - CalendarData.minBsYear;
+  const bsMonthData = CalendarData.extractedBsMonthData[bsMonth - 1];
+  for (let i = 0; i < bsMonthData.length; i++) {
+    if (bsMonthData[i] === 0) {
+      continue;
+    }
+
+    const bsMonthUpperDaysIndex = i % 2;
+    yearCount += bsMonthData[i];
+    if (totalYears <= yearCount) {
+      if (
+        (bsYear === 2085 && bsMonth === 5) ||
+        (bsYear === 2088 && bsMonth === 5)
+      ) {
+        return (
+          CalendarData.bsMonthUpperDays[bsMonth - 1][bsMonthUpperDaysIndex] - 2
+        );
+      } else {
+        return CalendarData.bsMonthUpperDays[bsMonth - 1][
+          bsMonthUpperDaysIndex
+        ];
+      }
+    }
+  }
+
+  return null;
+};
+
+export const convertADtoBS = (adYear, adMonth, adDate) => {
+  let bsYear = adYear + 57;
+  let bsMonth = (adMonth + 9) % 12;
+  bsMonth = bsMonth === 0 ? 12 : bsMonth;
+  let bsDate = 1;
+
+  if (adMonth < 4) {
+    bsYear -= 1;
+  }
+
+  const bsMonthFirstAdDate = convertBStoAD(bsYear, bsMonth, 1);
+  if (adDate >= 1 && adDate < bsMonthFirstAdDate.getDate()) {
+    if (adMonth === 4) {
+      const bsYearFirstAdDate = convertBStoAD(bsYear, 1, 1);
+      if (adDate < bsYearFirstAdDate.getDate()) {
+        bsYear -= 1;
+      }
+    }
+    bsMonth = bsMonth !== 1 ? bsMonth - 1 : 12;
+    const bsMonthDays = getBsMonthDays(bsYear, bsMonth);
+    if (bsMonthDays !== null) {
+      bsDate = bsMonthDays - (bsMonthFirstAdDate.getDate() - adDate) + 1;
+    }
+  } else {
+    bsDate = adDate - bsMonthFirstAdDate.getDate() + 1;
+  }
+  const dateFormat = `${toDevanagariDigits(bsYear)} ${
+    CalendarData.bsMonths[bsMonth - 1]
+  } ${toDevanagariDigits(bsDate)}`;
+  return { bsYear, bsMonth, bsDate, dateFormat };
+};
+
+export const convertBStoAD = (bsYear, bsMonth, bsDate) => {
+  if (bsYear === null || bsMonth === null || bsDate === null) {
+    return new Date();
+  }
+
+  const daysNumFromMinBsYear = getTotalDaysNumFromMinBsYear(
+    bsYear,
+    bsMonth,
+    bsDate
+  );
+  const adDate = new Date(
+    CalendarData.minAdDateEqBsDate.ad.year,
+    CalendarData.minAdDateEqBsDate.ad.month,
+    CalendarData.minAdDateEqBsDate.ad.date - 1
+  );
+  if (daysNumFromMinBsYear !== null) {
+    adDate.setDate(adDate.getDate() + daysNumFromMinBsYear);
+  }
+  return adDate;
+};
